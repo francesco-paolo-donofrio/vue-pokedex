4166a049ff247a10609a41ddc6dd3efb8a1baf25
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-14 15:28:17 +0200

    Store updated

--- src/store.js
+++ src/store.js
@@ -1,4 +1,13 @@
 import { reactive } from "vue";
 export const store = reactive({
+  state: {
     pokemonCollection: [],
-  });
+    currentPokemonName: ''
+  },
+  mutations: {
+    addPokemon(state, pokemon) {
+      state.pokemonCollection.push(pokemon);
+      state.currentPokemonName = pokemon.name; // Salva il nome dell'ultimo Pokémon aggiunto
+    }
+  },
+});
