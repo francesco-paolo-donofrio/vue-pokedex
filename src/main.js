c6db7098aab866fa070bde9deaf4310434d86ad9
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-17 11:16:30 +0200

    added bootstrap to main

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import './assets/styles/general.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
+import 'bootstrap/dist/css/bootstrap.css';    // Per gli stili di Bootstrap
+import 'bootstrap/dist/js/bootstrap.bundle.js'; // Per il JavaScript di Bootstrap (incluso Popper.js)
 
 createApp(App).mount('#app')
